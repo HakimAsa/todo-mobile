98b769c72d6220e40eb8fc24702592f0b9c1e4b8
aamidou <akimayena26@gmail.com>
2025-03-14 11:10:35 +0100

    prepare folders structure of the app

---
+++ app/components/inputs/TmTextInput.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TmTextInput() {
+  return (
+    <View>
+      <Text>TmTextInput</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
