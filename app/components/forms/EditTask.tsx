b0b67e2d48d5f2fe78fc64b643ad8d0e9371a101
aamidou <akimayena26@gmail.com>
2025-03-22 18:59:36 +0100

    fix warning: is missing the required default export on EditTask class

---
+++ app/components/forms/EditTask.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function EditTask() {
+  return (
+    <View>
+      <Text>Edit Task form</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
