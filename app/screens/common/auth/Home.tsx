5e5d94b8511b3c504dae0292e60bdd72e2b7d44b
aamidou <akimayena26@gmail.com>
2025-03-22 19:10:33 +0100

    fix warning: is missing the required default export on Home class

--- app/screens/common/auth/Home.tsx
+++ app/screens/common/auth/Home.tsx
@@ -1,1 +1,13 @@
 // display charts in the application and summary of tasks.
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>Home page after login</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
