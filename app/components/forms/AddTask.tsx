8cb1b9a3e11f75fafc44558ae4756eb8c67401ce
aamidou <akimayena26@gmail.com>
2025-03-22 18:58:09 +0100

    fix warning: is missing the required default export on AddTask class

---
+++ app/components/forms/AddTask.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AddTask() {
+  return (
+    <View>
+      <Text>Form to add task</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
