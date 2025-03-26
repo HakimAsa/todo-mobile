d2ad249eba1c3e5902f58f00b29a8bb8a989f758
aamidou <akimayena26@gmail.com>
2025-03-26 12:40:24 +0100

    start Welcome screen implementation

--- app/components/common/image/Logo.tsx
+++ app/components/common/image/Logo.tsx
@@ -4,22 +4,27 @@ interface ImageProps {
   marginTop?: number
   height?: number
   width?: number
+  style?: any
 }
 
 export default function Logo({
   marginTop,
+  style,
   height = 50,
   width = 50,
 }: ImageProps) {
   return (
     <Image
       source={require('../../../../assets/images/logo.png')}
-      style={{
-        width,
-        height,
-        borderRadius: width * 0.5,
-        marginTop: marginTop || 10,
-      }}
+      style={[
+        {
+          width,
+          height,
+          borderRadius: width * 0.5,
+          marginTop: marginTop || 10,
+        },
+        style,
+      ]}
     />
   )
 }
--- app/screens/common/auth/Welcome.tsx
+++ app/screens/common/auth/Welcome.tsx
@@ -1,12 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Colors from '@/app/config/colors'
+import Logo from '@/app/components/common/image/Logo'
+import TmText from '@/app/components/common/text/TmText'
 
 export default function Welcome() {
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <ImageBackground
+      style={styles.background}
+      source={require('@/assets/images/backgroundImage.png')}
+    >
+      <View style={styles.logoContainer}>
+        <Logo
+          width={100}
+          height={100}
+        />
+        <TmText
+          big
+          style={styles.welcomeText}
+        >
+          Manage Your Tasks Efficiently
+        </TmText>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    backgroundColor: Colors.primary,
+    height: 40,
+    width: '100%',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    position: 'absolute',
+    top: 70,
+  },
+  registerButton: {
+    backgroundColor: Colors.primary1,
+    height: 40,
+    width: '100%',
+  },
+  welcomeText: {
+    color: Colors.primary,
+    fontStyle: 'italic',
+    fontWeight: 900,
+  },
+})
