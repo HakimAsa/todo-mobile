e1ca985212f484d2816c249a8cb534a54241192d
aamidou <akimayena26@gmail.com>
2025-03-27 13:12:45 +0100

    implement KeyboardAvoidViewContainer to evict keyboard covering input

---
+++ app/components/containers/KeyboardAvoidViewContainer.tsx
@@ -0,0 +1,21 @@
+import { KeyboardAvoidingView, KeyboardAvoidingViewProps } from 'react-native'
+import React, { useContext } from 'react'
+import { HeaderHeightContext } from '@react-navigation/elements'
+
+import { onIOS } from '@/app/config/constants'
+
+interface KeyboardProps extends KeyboardAvoidingViewProps {}
+
+export default function KeyboardAvoidViewContainer({
+  children,
+}: KeyboardProps) {
+  return (
+    <KeyboardAvoidingView
+      behavior={onIOS ? 'padding' : undefined}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={useContext(HeaderHeightContext) ?? 0}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  )
+}
