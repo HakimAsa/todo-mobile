c87d49f4e255ae5f6200a83fcf0a6e8fffc97865
aamidou <akimayena26@gmail.com>
2025-05-03 16:21:12 +0100

    add custom useApi hook

---
+++ app/hooks/useApi.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { ApiResponse } from 'apisauce'
+
+import callServerError from '../utils/callServerError'
+
+type ApiFunction<T, Args extends any[]> = (
+  ...args: Args
+) => Promise<ApiResponse<T>>
+
+export default function useApi<T, Args extends any[]>(
+  apiFunc: ApiFunction<T, Args>
+) {
+  const [data, setData] = useState<T | null>(null)
+  const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
+  const [message, setMessage] = useState<string | null>(null)
+
+  const request = async (...args: Args) => {
+    setLoading(true)
+    const response = await apiFunc(...args)
+    setLoading(false)
+    setError(!response?.ok)
+    setData(response?.data ?? null)
+    setMessage(
+      !response
+        ? 'Unauthorized'
+        : callServerError(response) || 'An error occured!'
+    )
+
+    return response
+  }
+
+  return { data, error, loading, message, request, setLoading }
+}
---
+++ app/utils/callServerError.ts
@@ -0,0 +1,11 @@
+const callServerError = (error: any): string => {
+  if (typeof error === 'string') return error
+  if (error.response && error.response.data) {
+    if (typeof error.response.data === 'string') return error.response.data
+    return error.response.data.errorMessage || error.response.data.message
+  }
+  if (error.response?.message) return error.response.message
+  return error.message
+}
+
+export default callServerError
